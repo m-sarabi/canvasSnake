3aa890ab50846cd72c0f0990f457a2bf28426de4
m-sarabi <m.sarabi.jkd@gmail.com>
2023-08-12 22:50:21 +0330

    moving ball animation with redrawing functions

--- js/script.js
+++ js/script.js
@@ -10,20 +10,47 @@ canvas.addEventListener('contextmenu', function (event) {
     return false;
 }, false);
 
-ctx.beginPath();
-ctx.setLineDash([10, 2]);
-ctx.strokeStyle = '#0005';
-for (let i = cellSize; i < canvas.height; i += cellSize) {
-    ctx.moveTo(0, i);
-    ctx.lineTo(canvas.width, i);
+function drawGrid() {
+    ctx.beginPath();
+    ctx.setLineDash([10, 2]);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#0005';
+    for (let i = cellSize; i < canvas.height; i += cellSize) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+    }
+    for (let i = cellSize; i < canvas.width; i += cellSize) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+    }
+    ctx.stroke();
+    ctx.setLineDash([]);
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 5;
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    ctx.closePath();
 }
-for (let i = cellSize; i < canvas.width; i += cellSize) {
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, canvas.height);
+
+
+let x = 0, y = 0;
+let dx = 2, dy = 2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x + cellSize / 2, y + cellSize / 2, cellSize / 3, 0, Math.PI * 2);
+    ctx.fillStyle = 'orange';
+    ctx.fill();
+    ctx.closePath();
+
+    if (x < canvas.width - cellSize) {
+        x += dx;
+    }
 }
-ctx.stroke();
-ctx.setLineDash([]);
-ctx.strokeStyle = '#000';
-ctx.lineWidth = 5;
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
-ctx.closePath();
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+    drawBall();
+}
+
+setInterval(draw, 15);
